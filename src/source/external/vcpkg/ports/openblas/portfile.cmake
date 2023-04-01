87d24c9909580c457383840fad90949569a3e7ec
James Cahill <jcahill773@gmail.com>
2023-04-01 12:31:52 +0100

    (ports/openblas): PoC new buildscript for successful wasm32 build

--- src/source/external/vcpkg/ports/openblas/portfile.cmake
+++ src/source/external/vcpkg/ports/openblas/portfile.cmake
@@ -1,109 +1,52 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO xianyi/OpenBLAS
-    REF 63b03efc2af332c88b86d4fd8079d00f4b439adf # v0.3.10
-    SHA512 269852348e042fe32d0d400d5e6f4cf758024389d3966a9b1bc217061d4a03b3a7003a399212130ec4e783f1e1e5b423eb531e6e0948485b5d5ac9fdc58982cb
+    REF e46971b9d572a6cf040179b1d67e8408ce944a6f # v0.3.22
+    SHA512 2772afe2b73fe2b38bedc504b721d496b2303cc9159739e12129b38692836b8058bebc62699d3e1a36b90ef06d0919ed1cb9a0bf7349c2ef6f9958e47397890f
     HEAD_REF develop
-    PATCHES
-        uwp.patch
-        fix-space-path.patch
-        fix-redefinition-function.patch
-        fix-pkg-config.patch
-        fix-uwp-build.patch
 )
 
-find_program(GIT NAMES git git.cmd)
+foreach(BUILDTYPE "debug" "release")
+    if(BUILDTYPE STREQUAL "debug")
+        set(SHORT_BUILDTYPE "-dbg")
+        set(CMAKE_BUILDTYPE "DEBUG")
+        set(PATH_SUFFIX "/debug")
+    else()
+        if (_VCPKG_NO_DEBUG)
+            set(SHORT_BUILDTYPE "")
+        else()
+            set(SHORT_BUILDTYPE "-rel")
+        endif()
+        set(CMAKE_BUILDTYPE "RELEASE")
+        set(PATH_SUFFIX "")
+     endif()
 
-# sed and awk are installed with git but in a different directory
-get_filename_component(GIT_EXE_PATH ${GIT} DIRECTORY)
-set(SED_EXE_PATH "${GIT_EXE_PATH}/../usr/bin")
+    set(WORKING_DIRECTORY "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}${SHORT_BUILDTYPE}")
 
-# openblas require perl to generate .def for exports
-vcpkg_find_acquire_program(PERL)
-get_filename_component(PERL_EXE_PATH ${PERL} DIRECTORY)
-set(PATH_BACKUP "$ENV{PATH}")
-vcpkg_add_to_path("${PERL_EXE_PATH}")
-vcpkg_add_to_path("${SED_EXE_PATH}")
+    if (NOT EXISTS "${WORKING_DIRECTORY}")
+        file(MAKE_DIRECTORY "${WORKING_DIRECTORY}")
+    endif()
 
-set(COMMON_OPTIONS -DBUILD_WITHOUT_LAPACK=ON)
+    message(STATUS "Sourcepath: ${SOURCE_PATH}")
+    message(STATUS "Destination: ${WORKING_DIRECTORY}")
 
-# for UWP version, must build non uwp first for helper
-# binaries.
-if(VCPKG_TARGET_IS_UWP)
-    message(STATUS "Building Windows helper files")
-    set(TEMP_CMAKE_SYSTEM_NAME "${VCPKG_CMAKE_SYSTEM_NAME}")
-    set(TEMP_CMAKE_SYSTEM_VERSION "${VCPKG_CMAKE_SYSTEM_VERSION}")
-    set(TEMP_TARGET_TRIPLET "${TARGET_TRIPLET}")
-    unset(VCPKG_CMAKE_SYSTEM_NAME)
-    unset(VCPKG_CMAKE_SYSTEM_VERSION)
-    set(TARGET_TRIPLET "x64-windows")
+    file(COPY ${SOURCE_PATH}/ DESTINATION ${WORKING_DIRECTORY})
+endforeach()
 
-    vcpkg_configure_cmake(
-        SOURCE_PATH ${SOURCE_PATH}
-        OPTIONS
-            ${COMMON_OPTIONS}
-            -DTARGET=NEHALEM
-    )
-
-    # add just built path to environment for gen_config_h.exe,
-    # getarch.exe and getarch_2nd.exe
-    vcpkg_add_to_path("${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel")
-
-    # restore target build information
-    set(VCPKG_CMAKE_SYSTEM_NAME "${TEMP_CMAKE_SYSTEM_NAME}")
-    set(VCPKG_CMAKE_SYSTEM_VERSION "${TEMP_CMAKE_SYSTEM_VERSION}")
-    set(TARGET_TRIPLET "${TEMP_TARGET_TRIPLET}")
-
-    message(STATUS "Finished building Windows helper files")
-
-    vcpkg_configure_cmake(
-        SOURCE_PATH ${SOURCE_PATH}
-        OPTIONS
-            ${COMMON_OPTIONS}
-            -DCMAKE_SYSTEM_PROCESSOR=AMD64
-            -DVS_WINRT_COMPONENT=TRUE
-            "-DBLASHELPER_BINARY_DIR=${CURRENT_BUILDTREES_DIR}/x64-windows-rel")
-
-elseif(VCPKG_TARGET_IS_WINDOWS)
-    vcpkg_configure_cmake(
-        PREFER_NINJA
-        SOURCE_PATH ${SOURCE_PATH}
-        OPTIONS
-            ${COMMON_OPTIONS})
-else()
-    list(APPEND VCPKG_C_FLAGS "-DNEEDBUNDERSCORE") # Required to get common BLASFUNC to append extra _
-    list(APPEND VCPKG_CXX_FLAGS "-DNEEDBUNDERSCORE")
-    vcpkg_configure_cmake(
-        SOURCE_PATH ${SOURCE_PATH}
-        OPTIONS
-            ${COMMON_OPTIONS}
-            -DCMAKE_SYSTEM_PROCESSOR=AMD64
-            -DNOFORTRAN=ON
-            -DBU=_  #required for all blas functions to append extra _ using NAME
-            )
-endif()
+# NOTE: This appears to only build the release build type.
+vcpkg_execute_build_process(
+    COMMAND emmake make libs shared CC=emcc HOSTCC=gcc TARGET=RISCV64_GENERIC NOFORTRAN=1 USE_THREAD=0 -j${VCPKG_CONCURRENCY}
+    WORKING_DIRECTORY ${WORKING_DIRECTORY}
+    LOGNAME manual-${TARGET_TRIPLET}
+)
 
 
-vcpkg_install_cmake()
-vcpkg_fixup_cmake_targets(CONFIG_PATH share/cmake/OpenBLAS TARGET_PATH share/openblas)
-set(ENV{PATH} "${PATH_BACKUP}")
+file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/lib)
 
-set(pcfile "${CURRENT_PACKAGES_DIR}/lib/pkgconfig/openblas.pc")
-if(EXISTS "${pcfile}")
-    file(READ "${pcfile}" _contents)
-    set(_contents "prefix=${CURRENT_INSTALLED_DIR}\n${_contents}")
-    file(WRITE "${pcfile}" "${_contents}")
-    #file(CREATE_LINK "${pcfile}" "${CURRENT_PACKAGES_DIR}/lib/pkgconfig/blas.pc" COPY_ON_ERROR)
-endif()
-set(pcfile "${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig/openblas.pc")
-if(EXISTS "${pcfile}")
-    file(READ "${pcfile}" _contents)
-    set(_contents "prefix=${CURRENT_INSTALLED_DIR}/debug\n${_contents}")
-    file(WRITE "${pcfile}" "${_contents}")
-    #file(CREATE_LINK "${pcfile}" "${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig/blas.pc" COPY_ON_ERROR)
-endif()
-vcpkg_fixup_pkgconfig()
-#maybe we need also to write a wrapper inside share/blas to search implicitly for openblas, whenever we feel it's ready for its own -config.cmake file
+file(GLOB DYN_LIBS ${WORKING_DIRECTORY}/*.so*) # .so.0 for example
+file(GLOB STATIC_LIBS ${WORKING_DIRECTORY}/*.a)
+file(INSTALL ${DYN_LIBS} DESTINATION ${CURRENT_PACKAGES_DIR}/lib)
+file(INSTALL ${STATIC_LIBS} DESTINATION ${CURRENT_PACKAGES_DIR}/lib)
 
 # openblas do not make the config file , so I manually made this
 # but I think in most case, libraries will not include these files, they define their own used function prototypes
